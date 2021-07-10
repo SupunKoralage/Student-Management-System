79b4620b184734461bde2858b99987390906da6e
SupunKoralage <87217775+SupunKoralage@users.noreply.github.com>
2021-07-10 10:17:45 +0530

    Add files via upload

---
+++ stu/StudentReg/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace StudentReg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFee = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.comboCourse = new System.Windows.Forms.ComboBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.lblFee = new System.Windows.Forms.Label();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tblStudent = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edit = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(409, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Registation";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtFee);
+            this.groupBox1.Controls.Add(this.txtCourse);
+            this.groupBox1.Controls.Add(this.comboCourse);
+            this.groupBox1.Controls.Add(this.txtNo);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.lblCourse);
+            this.groupBox1.Controls.Add(this.lblFee);
+            this.groupBox1.Controls.Add(this.lblNo);
+            this.groupBox1.Controls.Add(this.lblName);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 303);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Registation";
+            // 
+            // txtFee
+            // 
+            this.txtFee.Location = new System.Drawing.Point(127, 258);
+            this.txtFee.Name = "txtFee";
+            this.txtFee.Size = new System.Drawing.Size(202, 24);
+            this.txtFee.TabIndex = 8;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(127, 140);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(202, 24);
+            this.txtCourse.TabIndex = 7;
+            // 
+            // comboCourse
+            // 
+            this.comboCourse.FormattingEnabled = true;
+            this.comboCourse.Items.AddRange(new object[] {
+            "Diploma in Java",
+            "Diploma in C#",
+            "Diploma in Python",
+            "Diploma in IT",
+            "Web Development",
+            "Database Managment"});
+            this.comboCourse.Location = new System.Drawing.Point(127, 197);
+            this.comboCourse.Name = "comboCourse";
+            this.comboCourse.Size = new System.Drawing.Size(202, 26);
+            this.comboCourse.TabIndex = 6;
+            this.comboCourse.SelectedIndexChanged += new System.EventHandler(this.comboCourse_SelectedIndexChanged);
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(127, 89);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(202, 24);
+            this.txtNo.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(127, 35);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(202, 24);
+            this.txtName.TabIndex = 5;
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold);
+            this.lblCourse.ForeColor = System.Drawing.Color.Black;
+            this.lblCourse.Location = new System.Drawing.Point(6, 147);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(54, 14);
+            this.lblCourse.TabIndex = 2;
+            this.lblCourse.Text = "Course";
+            // 
+            // lblFee
+            // 
+            this.lblFee.AutoSize = true;
+            this.lblFee.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold);
+            this.lblFee.ForeColor = System.Drawing.Color.Black;
+            this.lblFee.Location = new System.Drawing.Point(6, 265);
+            this.lblFee.Name = "lblFee";
+            this.lblFee.Size = new System.Drawing.Size(32, 14);
+            this.lblFee.TabIndex = 3;
+            this.lblFee.Text = "Fee";
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Font = new System.Drawing.Font("MS PGothic", 10F, System.Drawing.FontStyle.Bold);
+            this.lblNo.ForeColor = System.Drawing.Color.Black;
+            this.lblNo.Location = new System.Drawing.Point(6, 92);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(117, 14);
+            this.lblNo.TabIndex = 1;
+            this.lblNo.Text = "Contact Number";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("MS PGothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Black;
+            this.lblName.Location = new System.Drawing.Point(6, 42);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(99, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Student Name";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSave.Location = new System.Drawing.Point(42, 383);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(256, 383);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tblStudent
+            // 
+            this.tblStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Edit,
+            this.Delete});
+            this.tblStudent.Location = new System.Drawing.Point(374, 66);
+            this.tblStudent.Name = "tblStudent";
+            this.tblStudent.Size = new System.Drawing.Size(754, 291);
+            this.tblStudent.TabIndex = 11;
+            this.tblStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Student Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Tel.Number";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Course";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Fee";
+            this.Column5.Name = "Column5";
+            // 
+            // Edit
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edit.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Edit.HeaderText = "Edit";
+            this.Edit.Name = "Edit";
+            this.Edit.Text = "Edit";
+            this.Edit.UseColumnTextForButtonValue = true;
+            // 
+            // Delete
+            // 
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Delete.HeaderText = "Delete";
+            this.Delete.Name = "Delete";
+            this.Delete.Text = "Delete";
+            this.Delete.UseColumnTextForButtonValue = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnRefresh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnRefresh.Location = new System.Drawing.Point(1022, 383);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(106, 36);
+            this.btnRefresh.TabIndex = 12;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StudentReg.Properties.Resources.student1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1140, 478);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.tblStudent);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboCourse;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.Label lblFee;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView tblStudent;
+        private System.Windows.Forms.TextBox txtFee;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewButtonColumn Edit;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ stu/StudentReg/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StudentReg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+
+
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-91DD8R4\\SQLEXPRESS; Initial Catalog=abc; User Id=supun ; Password=supun; ");
+        SqlCommand cmd;
+        SqlDataReader read;
+        SqlDataAdapter drr;
+        string id;
+        bool Mode = true;
+        string sql;
+
+
+        
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sql = "select* from student";
+                cmd = new SqlCommand(sql, con);
+                con.Open();
+
+                read = cmd.ExecuteReader();
+
+                drr = new SqlDataAdapter(sql, con);
+                tblStudent.Rows.Clear();
+
+                while (read.Read())
+                {
+                    tblStudent.Rows.Add(read[0], read[1], read[2], read[3], read[4]);
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void getID(string id)
+        {
+           sql =  "select* from student where id = '" + id + "' ";
+            cmd = new SqlCommand(sql, con);
+            con.Open();
+            read = cmd.ExecuteReader();
+
+            while(read.Read())
+            {
+                txtName.Text = read[1].ToString();
+                txtNo.Text = read[4].ToString();
+                txtCourse.Text = read[2].ToString(); 
+                txtFee.Text = read[3].ToString();
+            }
+            con.Close();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string number = txtNo.Text;
+            string course = txtCourse.Text;
+            string fee = txtFee.Text;
+
+
+            if(Mode == true)
+            {
+                sql = "insert into student(stname,telno,course,fee) values(@stname,@telno,@course,@fee)";
+                con.Open();
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@telno", number);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                MessageBox.Show("Record Added successfull");
+                cmd.ExecuteNonQuery();
+
+
+                txtName.Clear();
+                txtNo.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+                txtName.Focus();  
+
+            } 
+            else
+            {
+                id = tblStudent.CurrentRow.Cells[0].Value.ToString();
+                sql = "update student set stname = @stname , telno = @telno , course = @course , fee = @fee where id =@id";
+                con.Open();
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@telno", number);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                cmd.Parameters.AddWithValue("@id", id);
+                MessageBox.Show("Record updated successfull");
+                cmd.ExecuteNonQuery();
+
+
+                txtName.Clear();
+                txtNo.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+                txtName.Focus();
+                btnSave.Text = "Save";
+                Mode = true;
+
+            }
+            con.Close();
+        }
+
+        private void comboCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtCourse.Text = comboCourse.Text;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == tblStudent.Columns["Edit"].Index && e.RowIndex >=0)
+            {
+                Mode = false;
+                id = tblStudent.CurrentRow.Cells[0].Value.ToString();
+                getID(id);
+                btnSave.Text = "Edit";
+            }
+            else if(e.ColumnIndex == tblStudent.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = tblStudent.CurrentRow.Cells[0].Value.ToString();
+                sql = "delete from student where id = @id";
+                con.Open();
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Delete Successufull");
+                con.Close();
+            }
+                       
+
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                sql = "select* from student";
+                cmd = new SqlCommand(sql, con);
+                con.Open();
+
+                read = cmd.ExecuteReader();
+
+                drr = new SqlDataAdapter(sql, con);
+                tblStudent.Rows.Clear();
+
+                while (read.Read())
+                {
+                    tblStudent.Rows.Add(read[0], read[1], read[2], read[3], read[4]);
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtNo.Clear();
+            txtCourse.Clear();
+            txtFee.Clear();
+            txtName.Focus();
+            btnSave.Text = "Save";
+            Mode = true;
+
+        }
+    }
+}
